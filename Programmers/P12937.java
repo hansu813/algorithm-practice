530b700c010df54ec2fc89c6592b8cf34668f05c
hansu813 <108872334+hansu813@users.noreply.github.com>
2026-02-04 10:36:15 +0900

    Add files via upload

---
+++ Programmers/P12937.java
@@ -0,0 +1,26 @@
+package Prgorammers;
+/*
+    [ 연습문제 - 짝수와 홀수 ]
+    문제 설명
+    정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
+
+    제한 조건
+    num은 int 범위의 정수입니다.
+    0은 짝수입니다.
+
+    입출력 예
+    num	return
+    3	"Odd"
+    4	"Even"
+ */
+
+public class P12937 {
+    public static String solution(int num) {
+        return num % 2 == 0 ? "Even" : "Odd";
+    }
+
+    static void main() {
+        System.out.println(solution(3));
+        System.out.println(solution(4));
+    }
+}
