0b53e846a4c90fe32df2e24b1769280e5ef4ba4c
hansu813 <108872334+hansu813@users.noreply.github.com>
2026-04-21 17:31:09 +0900

    Add files via upload

---
+++ Programmers/S12945.java
@@ -0,0 +1,44 @@
+package Prgorammers;
+/*
+    [연습문제 - 피보나치 수]
+    피보나치 수는 F(0) = 0, F(1) = 1일 때, 1 이상의 n에 대하여 F(n) = F(n-1) + F(n-2) 가 적용되는 수 입니다.
+
+예를들어
+
+F(2) = F(0) + F(1) = 0 + 1 = 1
+F(3) = F(1) + F(2) = 1 + 1 = 2
+F(4) = F(2) + F(3) = 1 + 2 = 3
+F(5) = F(3) + F(4) = 2 + 3 = 5
+와 같이 이어집니다.
+
+2 이상의 n이 입력되었을 때, n번째 피보나치 수를 1234567으로 나눈 나머지를 리턴하는 함수, solution을 완성해 주세요.
+
+제한 사항
+n은 2 이상 100,000 이하인 자연수입니다.
+입출력 예
+n	return
+3	2
+5	5
+입출력 예 설명
+ */
+public class S12945 {
+    public int solution(int n) {
+        int[] answer = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            if (i == 0) {
+                answer[i] = 0;
+            } else if (i == 1) {
+                answer[i] = 1;
+            } else {
+                int sum = answer[i - 2] + answer[i - 1];
+                answer[i] = sum % 1234567;
+            }
+        }
+        return answer[n];
+    }
+
+    static void main(String[] args) {
+
+    }
+}
